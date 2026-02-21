fd026ebd5d01030a401baa018364bd7507f1249d
rudolffu <fuympku@outlook.com>
2026-02-21 17:14:19 +0100

    Configure PyPI publish workflow and bump version metadata

--- euclidkit/version.py
+++ euclidkit/version.py
@@ -1,10 +1,10 @@
 """Version information for euclidkit package."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0rc1"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Version metadata
 __author__ = "Yuming Fu"
 __email__ = "fuympku@outlook.com"
 __license__ = "GPLv3"
-__copyright__ = "Copyright 2025, Yuming Fu"
+__copyright__ = "Copyright 2026, Yuming Fu"
